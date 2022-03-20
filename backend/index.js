86a41b28a42fbb92268050015b322c2e3aa718f3
Batya <Gigi.ucla21@gmail.com>
2022-03-20 14:19:25 -0700

    first commit with new schema

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+//CREATE MONGO CONNECTION
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/soccorDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//SETUP BODY PARSER
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) =>
+    res.send(`This application is running on ${PORT}`)
+);
+
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
